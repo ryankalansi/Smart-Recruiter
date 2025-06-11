b47feae254f0589515320ad3985abdac97579e1e
ryan <kalansiryan@gmail.com>
2025-06-11 22:13:31 +0700

    delete resultpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import UploadPage from "./pages/UploadPage";
-import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/upload" element={<UploadPage />} />
-          <Route path="/result/:id" element={<ResultPage />} />
         </Routes>
       </div>
     </Router>
